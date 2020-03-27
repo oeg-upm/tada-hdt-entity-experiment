5b6d352e6ea8ff55e46666af4b30a3bc551cf22e
Ahmad Alobaid <ahmad88me@gmail.com>
2020-03-27 03:10:30 +0100

    update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,16 +13,16 @@ rm  v1.0.zip
 cd easy-logger-1.0;make install;cd ..;rm -Rf easy-logger-1.0
 
 echo "Installing tabular-parser... "
-wget https://github.com/ahmad88me/tabular-parser/archive/v1.1.zip
-unzip v1.1.zip
-rm v1.1.zip
-cd tabular-parser-1.1;make install;cd ..;rm -Rf tabular-parser-1.1
-
-echo "Installing tada-hdt-entity... "
-wget https://github.com/oeg-upm/tada-hdt-entity/archive/v1.2.zip
+wget https://github.com/ahmad88me/tabular-parser/archive/v1.2.zip
 unzip v1.2.zip
 rm v1.2.zip
-cd tada-hdt-entity-1.2;make install;cd ..;rm -Rf tada-hdt-entity-1.2
+cd tabular-parser-1.2;make install;cd ..;rm -Rf tabular-parser-1.2
+
+echo "Installing tada-hdt-entity... "
+wget https://github.com/oeg-upm/tada-hdt-entity/archive/v1.3.zip
+unzip v1.3.zip
+rm v1.3.zip
+cd tada-hdt-entity-1.3;make install;cd ..;rm -Rf tada-hdt-entity-1.3
 
 echo "update linker caches..."
 ldconfig
